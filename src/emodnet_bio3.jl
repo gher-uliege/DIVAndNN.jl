f365cc9f113ee7971aceef910cf8e5a07f452483
Alexander Barth <barth.alexander@gmail.com>
2018-10-09 14:49:14 +0200

    wip

--- src/emodnet_bio3.jl
+++ src/emodnet_bio3.jl
@@ -4,7 +4,6 @@ import DIVAnd
 using NCDatasets
 using Missings
 using Interpolations
-using MAT
 
 if VERSION >= v"0.7"
     using Random
@@ -16,10 +15,6 @@ else
     using Compat: @info, @warn, range, cat
 end
 
-function mad(x)
-    sel = .!isnan.(x)
-    return median(abs.(x[sel] - median(x[sel])))
-end
 
 function interpcv(xyi,value_analysis,xy)
     ncv = length(xy[1])
@@ -70,7 +65,6 @@ end
 
 
 function binobs(xy,v,XY)
-
     sz = size(XY[1])
     n = length(sz)
     sumv = zeros(sz)
@@ -99,23 +93,14 @@ function binobs(xy,v,XY)
     return meanv
 end
 
-
-function surfacemean(fname,varname)
-    step = 2
-
-    ds = Dataset(fname)
-    S = ds[varname][:,:,end,:];
-    lon = nomissing(ds["lon"][1:step:end])
-    lat = nomissing(ds["lat"][1:step:end])
-    SS = nomissing(S);
-    SS[ismissing.(S)] .= NaN;
-    close(ds)
-    return lon,lat,mean(SS,3)[1:step:end,1:step:end,1]
-end
-
 coast() = contourf(bx,by,b' .> 0, levels=[0,.5], cmap = "gray")
 
 include("DIVAnd_covar.jl")
+include("emodnet_bio_grid.jl")
+include("emodnet_bio_loadobs.jl")
+
+outdir = joinpath(datadir,"Results","Zooplankton")
+mkpath(outdir)
 
 if VERSION >= v"0.7"
     Random.seed!(1234)
@@ -123,43 +108,77 @@ else
     srand(1234)
 end
 
-datadir = get(ENV,"DATADIR",expanduser("~/tmp/Emodnet-Bio"))
 
+
+# land-sea mask and domain parameters
+
+maskname = joinpath(datadir,"mask.nc");
+
+ds = Dataset(maskname,"r")
+mask = nomissing(ds["mask"][:,:]) .== 1
+close(ds)
+mask = repeat(mask,inner=(1,1,length(years)))
 bathname = joinpath(datadir,"gebco_30sec_4.nc");
-
-
 bathisglobal = true;
+mask2,(pm,pn,po),(xi,yi,zi) = DIVAnd.domain(bathname,bathisglobal,gridlon,gridlat,years);
 
-#df = CSV.read(joinpath(datadir,"20180410_232725_15acd2c3d3150f.csv"))
-# measurement
-#df = CSV.read(joinpath(datadir,"20180503_144507_15aeb04539abf4.csv"))
 
-data,header = readdlm(joinpath(datadir,"outalex.csv"),',',header = true)
-header = header[:]
+# load covariables
 
-lon = Vector{Float64}(data[:,findfirst(header .== "decimalLongitude")])
-lat = Vector{Float64}(data[:,findfirst(header .== "decimalLatitude")])
-sname = "Acartia bifilosa"
+covars_fname = [("bathymetry.nc","batymetry",identity),
+                ("dist2coast_subset.nc","distance",identity),
+                #("Chlorophyll/chloro_reinterp.nc","chla",identity),
+                ("salinity.nc","salinity",log),
+                ("temperature.nc","temperature",identity)]
 
-for sname in header[6:end-1]
+sz = (length(gridlon),length(gridlat),length(years),length(covars_fname)+2)
+field = zeros(sz)
+
+for i = 1:length(covars_fname)
+    (fname,varname,trans) = covars_fname[i]
+
+    Dataset(joinpath(datadir,fname)) do ds
+        global tmp
+        tmp = nomissing(ds[varname][:],NaN)
+        tmp = trans.(tmp)
+        if ndims(tmp) == 2
+            field[:,:,:,i] = repeat(tmp,inner = (1,1,length(years)))
+        else
+            field[:,:,:,i] = tmp
+        end
+    end
+end
+
+X,Y = DIVAnd.ndgrid(gridlon,gridlat)
+
+field[:,:,:,end-1] = repeat(X,inner = (1,1,length(years)))
+field[:,:,:,end]   = repeat(Y,inner = (1,1,length(years)))
+
+
+# normalize
+
+for n = 1:size(field,4)
+    tmp = field[:,:,:,n][mask];
+    field[:,:,:,n] = (field[:,:,:,n] .- mean(tmp))./std(tmp)
+end
+
+
+fname = joinpath(datadir,"balticZooplankton.csv")
+
+
+scientificname_accepted = listnames(fname);
+
+
+sname = scientificname_accepted[1]
+#for sname in scientificname_accepted
+
     @show sname
+    lon,lat,obstime,value,ids = loadbyname(fname,years,sname)
  
-value = Vector{Float64}(data[:,findfirst(header .== sname)])
-obstime = DateTime.(data[:,findfirst(header .== "date")])
-time = Float64.(Dates.year.(obstime))
+    time = Float64.(Dates.year.(obstime))
 
     @show value[1:min(end,10)]
-
-#year = 2007;
-#year = 2010;
-#years = 2000:2012
-years = 2007:2013
-
-#for year in years
-
-sel = (years[1] .<= Dates.year.(obstime)  .<= years[end]) .& .!ismissing(value);
-
-@show  years,sum(sel)
+    @show length(value)
 
 #=
 sum(sel)
@@ -176,132 +195,12 @@ scatter(lon[sel],lat[sel],10,value[sel]; cmap = "jet"); colorbar()
 
 
 
-if false
-    fname = "http://sdn.oceanbrowser.net:8081/data/SeaDataNet-domains/Baltic/Salinity.19002012.4Danl.nc"
-    varname = "Salinity"
-    Slon,Slat,S = surfacemean(fname,varname)
-
-    fname = "http://sdn.oceanbrowser.net:8081/data/SeaDataNet-domains/Baltic/Temperature.19002012.4Danl.nc"
-    varname = "Temperature"
-    Tlon,Tlat,T = surfacemean(fname,varname)
-
-    ds = Dataset(expanduser("~/workspace/ContourExtractor/data/dist2coast.nc"));
-    Dlon = nomissing(ds["lon"][:])
-    Dlat = nomissing(ds["lat"][:])
-
-    i0,i1 = extrema(find(Tlon[1]-1 .<= Dlon .<= Tlon[end]+1))
-    j0,j1 = extrema(find(Tlat[1]-1 .<= Dlat .<= Tlat[end]+1))
-
-    D = nomissing(ds["distance2coast"][i0:i1,j0:j1]);
-
-
-    itp = interpolate((Dlon[i0:i1],Dlat[j1:-1:j0]), D[:,end:-1:1], Gridded(Linear()));
-    Di = itp[Tlon,Tlat];
-
-    figure(figsize=(11,8))
-    subplot(2,2,1)
-    pcolor(Tlon,Tlat,T')
-    title("Surface mean temperature")
-    colorbar()
-
-    subplot(2,2,2)
-    pcolor(Slon,Slat,S')
-    title("Surface mean salinity")
-    colorbar()
-
-    subplot(2,2,3)
-    Di[Di .< 0] .= NaN
-    pcolor(Tlon,Tlat,Di')
-    title("Distance from coast")
-    colorbar()
-
-
-    blon,blat,bath = DIVAnd.load_bath(bathname,bathisglobal,Tlon,Tlat);
-
-    subplot(2,2,4)
-    bath[bath .< 0] .= NaN;
-    pcolor(Tlon,Tlat,bath')
-    title("Depth")
-    colorbar()
-
-
-    MAT.matwrite(joinpath(datadir,"covariables.mat"),
-                 Dict("bath" => bath,
-                      "T" => T,
-                      "S" => S,
-                      "Di" => Di,
-                      "Tlon" => Tlon,
-                      "Tlat" => Tlat))
-else
-    dd = MAT.matread(joinpath(datadir,"covariables.mat"))
-
-    bath = dd["bath"]
-    T = dd["T"]
-    S = dd["S"]
-    Di = dd["Di"]
-    Tlon = dd["Tlon"]
-    Tlat = dd["Tlat"]
-end
-
-# for plotting
-bx,by,b = DIVAnd.extract_bath(bathname,bathisglobal,Tlon,Tlat);
-
-dd = matread(joinpath(datadir,"mean2017.mat"))
-Clon = dd["lon"];
-Clat = dd["lat"];
-chl = Float64.(dd["chl"]);
-
-itp = interpolate((Clon,Clat), chl[:,:,1], Gridded(Linear()));
-if VERSION >= v"0.7"
-    itp = extrapolate(itp,Line());
-end
-chli = itp(Tlon,Tlat);
-minc = minimum(chl[.!isnan.(chl)])
-chli[chli .<= minc] .= NaN;
-
-
-O2 = matread(joinpath(datadir,"covariables_O2_mean.mat"))["O2"]
-
-
-X,Y = DIVAnd.ndgrid(Tlon,Tlat)
-
-field = cat(T,log.(S),bath,Di,X,Y, dims = 3)
-#field = cat(T,S,bath,Di,X,Y, dims = 3)
-#field = cat(T,S, dims = 3)
-
-field = cat(T,S,bath,Di,X,Y,log.(chli), dims = 3)
-#field = cat(T,S,bath,Di, dims = 3)
-
-#field = cat(T,log.(S),bath,Di,O2,X,Y, dims = 3)
-
-
-mask = .!isnan.(bath);
-mask[1:30,70:end] .= false;
-mask[1:4,1:12] .= false;
-#mask[:] = true
-
-mask[(19.6 .<= X .<= 20.2) .& (63.4 .<= Y .<= 63.8)] .= true
-
-#pcolor(mask')
-
-fieldf = DIVAnd.ufill(field, .!isnan.(field));
-
-# normalize
-
-for k = 1:size(fieldf,3)
-    tmp = fieldf[:,:,k][mask];
-    fieldf[:,:,k] = (fieldf[:,:,k] .- mean(tmp))./std(tmp)
-end
-
-# 3D extension
-mask = repeat(mask,inner=(1,1,length(years)))
-fieldf = repeat(reshape(fieldf,size(bath)...,1,size(fieldf,3)),outer=(1,1,length(years),1));
 
 trans,invtrans = DIVAnd.Anam.loglin(30, epsilon = 1);
 #trans,invtrans = DIVAnd.Anam.notransform();
 
 if false
-    pcolor(fieldf[:,:,1]')
+    pcolor(field[:,:,1]')
 
 
 
@@ -318,16 +217,16 @@ if false
 
 end
 
-mask2,(pm,pn,po),(xi,yi,zi) = DIVAnd.domain(bathname,bathisglobal,Tlon,Tlat,years)
 
-XY = DIVAnd.ndgrid(Tlon,Tlat)
+#=
+XY = DIVAnd.ndgrid(gridlon,gridlat)
 
 xy = (lon[sel],lat[sel],time[sel]);
 v = value[sel];
 meanv = binobs(xy,v,XY);
 
 
-
+=#
 Ntries = 1
 
 
@@ -338,20 +237,21 @@ RMS_diva_covar = zeros(Ntries)
 
 epsilon2 = 0.5
 
-trans_value = trans.(value[sel])
+trans_value = trans.(value)
 mvalue = mean(trans_value)
+nsamp = 0
 
 varbak,len,dbinfo = DIVAnd.fitlen(
-    (lon[sel],lat[sel]),trans_value,0;
+    (lon,lat),trans_value,nsamp;
     distfun = DIVAnd.distfun_m)
 
-trans_value = trans.(value[sel])
+trans_value = trans.(value)
 mvalue = mean(trans_value)
 
 f = trans_value .- mvalue
 
 bestfactorl,bestfactore, cvval,cvvalues, x2Ddata,y2Ddata,cvinter,xi2D,yi2D =
-    DIVAnd.DIVAnd_cv(mask[:,:,1],(pm[:,:,1],pn[:,:,1]),(xi[:,:,1],yi[:,:,1]),(lon[sel],lat[sel]),f,len,epsilon2,2,3,0);
+    DIVAnd.DIVAnd_cv(mask[:,:,1],(pm[:,:,1],pn[:,:,1]),(xi[:,:,1],yi[:,:,1]),(lon,lat),f,len,epsilon2,2,3,0);
 
 #@warn "fix bestfactore"
 #bestfactore = 0.2438612845535494
@@ -374,28 +274,27 @@ value_analysis2 = zeros(size(mask))
 
 lent = 0.6 # years
 niter = 10000
-niter = 100000
-#niter = 10
+#niter = 100000
+niter = 10
 
 #for l = 1:Ntries
     l=1
 
     # 5% for cross-validation
-    selcv = rand(Float64,size(sel)) .< 0.10
-    #selcv = rand(Float64,size(sel)) .< -0.10
-    @show sum(sel .& selcv)
+    selcv = rand(Float64,size(value)) .< 0.10
+    @show sum(selcv)
 
     #    value_analysis,RMS_diva[l],value_analysis2,RMS_diva_covar[l] = comp(selcv)
 
-    lon_a = lon[sel .& .!selcv]
-    lat_a = lat[sel .& .!selcv]
-    time_a = time[sel .& .!selcv]
-    value_a = Vector{Float64}(value[sel .& .!selcv])
+    lon_a = lon[.!selcv]
+    lat_a = lat[.!selcv]
+    time_a = time[.!selcv]
+    value_a = Vector{Float64}(value[.!selcv])
 
-    lon_cv = lon[sel .& selcv]
-    lat_cv = lat[sel .& selcv]
-    time_cv = time[sel .& selcv]
-    value_cv = Vector{Float64}(value[sel .& selcv])
+    lon_cv = lon[selcv]
+    lat_cv = lat[selcv]
+    time_cv = time[selcv]
+    value_cv = Vector{Float64}(value[selcv])
 
     trans_value_a = trans.(value_a)
     mvalue = mean(trans_value_a)
@@ -409,10 +308,12 @@ niter = 100000
         alphabc = 0,
     );
 
+    cpme = DIVAnd_cpme(mask,(pm,pn,po),(xi,yi,zi),(lon_a,lat_a,time_a),f,(len,len,lent),epsilon2)
+
     value_analysis .= invtrans.(value_analysis .+ mvalue);
 
-    #lon[sel],lat[sel],10,value[sel]
-    RMS_diva[l] = validate((Tlon,Tlat,years),value_analysis,
+    #lon,lat,10,value
+    RMS_diva[l] = validate((gridlon,gridlat,years),value_analysis,
     		           (lon_cv,lat_cv,time_cv),value_cv)
 
     #epsilon2ap = 0.4
@@ -420,14 +321,14 @@ niter = 100000
     epsilon2ap = 2
     #epsilon2ap = 5
     #epsilon2ap = 50
-    #@show std(value[sel])
+    #@show std(value)
     epsilon2ap = epsilon2
 
-    #NLayers = [size(fieldf)[end],3,1]
-    NLayers = [size(fieldf)[end],4,1]
-    #NLayers = [size(fieldf)[end],5,1]
-    #NLayers = [size(fieldf)[end],2,1]
-    #NLayers = [size(fieldf)[end],1]
+    #NLayers = [size(field)[end],3,1]
+    NLayers = [size(field)[end],4,1]
+    #NLayers = [size(field)[end],5,1]
+    #NLayers = [size(field)[end],2,1]
+    #NLayers = [size(field)[end],1]
     #NLayers = []
     RMS_diva_covar_his = Float64[]
     stdf = std(f)
@@ -436,7 +337,7 @@ niter = 100000
         mask,(pm,pn,po),(xi,yi,zi),(lon_a,lat_a,time_a),
         f / stdf,
         (len,len,lent),epsilon2ap,
-        fieldf,
+        field,
         NLayers,
         costfun = regression,
         niter = niter,
@@ -445,7 +346,7 @@ niter = 100000
 	    plotres = (i,lossi,field,y) -> begin
             #@show i
 	        value_analysis2 = invtrans.(stdf * field .+ mvalue)
-    	    RMS = validate((Tlon,Tlat,years),value_analysis2,
+    	    RMS = validate((gridlon,gridlat,years),value_analysis2,
     	    	           (lon_cv,lat_cv,time_cv),value_cv)
             push!(RMS_diva_covar_his,RMS)
 	        @printf("| %10d | %30.5f | %30.5f | %30.5f |\n",i,lossi,RMS,1 - RMS^2/mean(RMS_diva)^2)
@@ -455,7 +356,7 @@ niter = 100000
 
     value_analysis2 .= invtrans.(stdf * value_analysis2 .+ mvalue)
 
-    RMS_diva_covar[l] = validate((Tlon,Tlat,years),value_analysis2,
+    RMS_diva_covar[l] = validate((gridlon,gridlat,years),value_analysis2,
     		           (lon_cv,lat_cv,time_cv),value_cv)
 
     #return value_analysis,RMS_diva,value_analysis2,RMS_diva_covar
@@ -463,46 +364,29 @@ niter = 100000
 #end
 
 
-#value_analysis,RMS_diva_,value_analysis2,RMS_diva_covar_ = comp(falses(size(sel)))
-
 
 @show mean(RMS_diva), mean(RMS_diva_covar)
 @show 1 - mean(RMS_diva_covar)^2/mean(RMS_diva)^2
 
 
-outname = "analysis-$(sname).nc"
+ncvarattrib = Dict("long_name" => "abundance of $(sname)",
+                   "units" => "1/m2",
+                   "comments" => "number per m2"
+                   )
 
-DIVAnd.save(outname,(Tlon,Tlat,DateTime.(years,1,1)),value_analysis2,"abundance")
-
-
-end
-
-#=
-skill
-epsilon2ap = 2
-NLayers = [size(fieldf)[end],4,1]
-L2reg = 0.,
-skill = 0.08603493221507186
+outname = joinpath(outdir,"DIVAnd-analysis-$(sname).nc")
+DIVAnd.save(outname,(gridlon,gridlat,DateTime.(years,1,1)),value_analysis,"abundance"; 
+            relerr = cpme, ncvarattrib = ncvarattrib)
 
 
-epsilon2ap = 1.5
-skill = 0.08600723001956245
-
-
-NLayers = [size(fieldf)[end],3,1]
-epsilon2ap = 2
-skill = 0.08553042651362863
+outname = joinpath(outdir,"DIVAndNN-analysis-$(sname).nc")
+DIVAnd.save(outname,(gridlon,gridlat,DateTime.(years,1,1)),value_analysis2,"abundance"; 
+            relerr = cpme, ncvarattrib = ncvarattrib)
 
 
 
-epsilon2ap = 2
-NLayers = [size(fieldf)[end],4,1]
-L2reg = 0.,
-skill = 0.08592267660472175
-
-
-with O2
-0.08585405392982948
-=#
 
 #end
+
+
+
---
+++ src/emodnet_bio_prep_mask.jl
@@ -0,0 +1,54 @@
+using DIVAnd
+using Interpolations
+using NCDatasets
+
+include("emodnet_bio_grid.jl");
+
+# land-sea mask and domain parameters
+
+bathname = joinpath(datadir,"gebco_30sec_4.nc");
+bathisglobal = true;
+mask2,(pm,pn,po),(xi,yi,zi) = DIVAnd.domain(bathname,bathisglobal,gridlon,gridlat,years);
+blon,blat,bath = DIVAnd.load_bath(bathname,bathisglobal,gridlon,gridlat);
+mask = (bath .>= 0);
+#mask[1:30,70:end] .= false;
+#mask[1:4,1:12] .= false;
+#mask[:] = true
+X,Y = DIVAnd.ndgrid(gridlon,gridlat)
+mask[(X .<= 15.) .& (62 .<= Y)] .= false
+mask[(X .<= 10.) .& (Y .<= 54)] .= false
+mask[(X .<= 9.3) .& (Y .<= 56)] .= false
+
+mask = DIVAnd.floodfill(mask,CartesianIndex(50,10))
+
+fname = joinpath(datadir,"mask.nc")
+
+ds = Dataset(fname,"c")
+# Dimensions
+
+ds.dim["lon"] = length(gridlon)
+ds.dim["lat"] = length(gridlat)
+
+# Declare variables
+
+nclon = defVar(ds,"lon", Float64, ("lon",)) 
+nclon.attrib["units"] = "degrees_east"
+nclon.attrib["standard_name"] = "longitude"
+nclon.attrib["long_name"] = "longitude"
+
+nclat = defVar(ds,"lat", Float64, ("lat",)) 
+nclat.attrib["units"] = "degrees_north"
+nclat.attrib["standard_name"] = "latitude"
+nclat.attrib["long_name"] = "latitude"
+
+ncmask = defVar(ds,"mask", Int8, ("lon", "lat")) 
+ncmask.attrib["comment"] = "1 sea; 0 land"
+
+
+# Define variables
+
+nclon[:] = blon
+nclat[:] = blat
+ncmask[:] = mask
+
+close(ds)

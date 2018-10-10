1ee6c84bd947b6c211da7756b7ab3d4a85d1f0e0
ctroupin <charles.troupin@gmail.com>
2018-10-10 09:17:39 +0200

    store benthos grid

--- src/emodnet_bio_grid.jl
+++ src/emodnet_bio_grid.jl
@@ -1,8 +1,13 @@
 
-# bounds
+# bounds for the Baltic Sea
 gridlon = 9.0 : 0.1 : 30.8
 gridlat = 53.0 : 0.1 : 66.1
 
 datadir = get(ENV,"DATADIR",expanduser("~/tmp/Emodnet-Bio"))
 
 years = 2007:2013
+
+# bounds for the Benthos product (Atlantic)
+dlon, dlat = 1/10., 1/10.
+gridlonBenthos = -10. : dlon : 35.
+gridlatBenthos = 36. : dlat : 73.

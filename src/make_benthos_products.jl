3ce54f5f6f32b667d7346fc6955defb47286fe79
ctroupin <charles.troupin@gmail.com>
2018-11-05 22:16:07 +0100

    plotting functions

--- src/make_benthos_products.jl
+++ src/make_benthos_products.jl
@@ -44,7 +44,7 @@ end
 
 """
 ```julia-repl
-obslon, obslat, g1, g2, g3 = read_benthos(filename)
+obslon, obslat, g1, g2, g3 = read_benthos_abs(filename)
 ```
 Read the data from the benthos data file with column as follows:
 data,x,y,sta,g1.abs,g2.abs,g3.abs,g1.rel,g2.rel,g3.rel
@@ -191,6 +191,104 @@ function write_benthos_nc(filename::String, gridlon, gridlat,
     end
 end;
 
+
+"""
+```julia-repl
+write_benthos_nc(filename, gridlon, gridlat, field1, field2, field3, err1, err2, err3)
+```
+Write the result of the analysis (`make_analysis`) and of the error computation (`compute_error`)
+in a netCDF file `filename`.
+"""
+function write_benthos_specific_nc(filename::String, gridlon, gridlat,
+        field1::Array, field2::Array, field3::Array,
+        err1::Array, err2::Array, err3::Array)
+
+    Dataset(filename,"c") do ds
+
+        nlon = length(gridlon);
+        nlat = length(gridlat);
+
+        # Define the dimension "lon" and "lat" with the size 100 and 110 resp.
+        defDim(ds,"lon",nlon);
+        defDim(ds,"lat",nlat);
+
+        # Define a global attribute
+        ds.attrib["title"] = "Interpolated Benthos - Specific Cases"
+
+        # Define the variables and coordinates
+        lon = defVar(ds,"lon",Float32,("lon",))
+        lat = defVar(ds,"lat",Float32,("lat",))
+
+        # Attributes
+        lat.attrib["long_name"] = "Latitude";
+        lat.attrib["standard_name"] = "latitude";
+        lat.attrib["units"] = "degrees_north";
+
+        lon.attrib["long_name"] = "Longitude";
+        lon.attrib["standard_name"] = "longitude";
+        lon.attrib["units"] = "degrees_east";
+
+        g1.attrib["long_name"] = "Arctica islandica";
+        g2.attrib["long_name"] = "Spiophanes bombyx";
+        g3.attrib["long_name"] = "Urothoe poseidonis";
+
+        # Interpolated fields
+        g1 = defVar(ds,"g1",Float64,("lon","lat"))
+        g2 = defVar(ds,"g2",Float64,("lon","lat"))
+        g3 = defVar(ds,"g3",Float64,("lon","lat"))
+
+        # Error fields
+        g1_err = defVar(ds,"g1_err",Float64,("lon","lat"))
+        g2_err = defVar(ds,"g2_err",Float64,("lon","lat"))
+        g3_err = defVar(ds,"g3_err",Float64,("lon","lat"))
+
+        # Fill the coord vectors and the fields
+        lon[:] = gridlon;
+        lat[:] = gridlat;
+
+        g1[:,:] = field1;
+        g2[:,:] = field2;
+        g3[:,:] = field3;
+
+        g1_err[:,:] = err1;
+        g2_err[:,:] = err2;
+        g3_err[:,:] = err3;
+
+    end
+end;
+
+"""
+```julia-repl
+make_scatter_grid(g1, g2, g3)
+```
+Create a figure with 3 subplots showing a scatter plot of the data, type-by-type
+"""
+function make_scatter_grid(g1, g2, g3)
+    figure("benthos_data", figsize=(12,8))
+    ax1 = subplot(1,3,1)
+    title("Resistant", fontsize=8)
+    #PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax1[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    scat1 = PyPlot.scatter(obslon, obslat, s=0.5, c=g1)
+    cb = colorbar(scat1, shrink=.5)[:ax][:tick_params](labelsize=8)
+    ax2 = subplot(1,3,2)
+    title("Resilient", fontsize=8)
+    #PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax2[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    scat2 = PyPlot.scatter(obslon, obslat, s=0.5, c=g2)
+    colorbar(scat2, shrink=.5)[:ax][:tick_params](labelsize=8)
+    ax3 = subplot(1,3,3)
+    title("Vulnerable", fontsize=8)
+    #PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax3[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    scat3 = PyPlot.scatter(obslon, obslat, s=0.5, c=g3)
+    colorbar(scat3,shrink=.5)[:ax][:tick_params](labelsize=8)
+end
+
+
 """
 ```julia-repl
 make_plot_grid(field1, field2, field3, fieldtotal; vmin, vmax)
@@ -239,7 +337,7 @@ make_plot_grid3(field1, field2, field3, vmin, vmax)
 Create a figure with the 3 fields side-by-side.
 vmin and vmax are applied as the lower and upper limits for the field values.
 """
-function make_plot_grid3(field1, field2, field3; vmin=0, vmax=1.)
+function make_plot_grid3(field1, field2, field3; vmin=0, vmax=1., shrink=1)
     figure("benthos_results", figsize=(12,8))
     ax1 = subplot(1,3,1)
     title("Resistant", fontsize=8)
@@ -248,7 +346,7 @@ function make_plot_grid3(field1, field2, field3; vmin=0, vmax=1.)
     ax1[:tick_params]("both",labelsize=6)
     gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
     #PyPlot.scatter(obslon, obslat, s=0.5, c=g1log, vmin=0, vmax=10.)
-    cb = colorbar(pcm1)[:ax][:tick_params](labelsize=8)
+    cb = colorbar(pcm1,shrink=shrink)[:ax][:tick_params](labelsize=8)
     ax2 = subplot(1,3,2)
     title("Resilient", fontsize=8)
     pcm2 = pcolormesh(gridlonBenthos, gridlatBenthos, permutedims(field2, [2,1]), vmin=vmin, vmax=vmax)
@@ -256,7 +354,7 @@ function make_plot_grid3(field1, field2, field3; vmin=0, vmax=1.)
     ax2[:tick_params]("both",labelsize=6)
     gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
     #PyPlot.scatter(obslon, obslat, s=0.5, c=g2log, vmin=0, vmax=10.)
-    colorbar(pcm2)[:ax][:tick_params](labelsize=8)
+    colorbar(pcm2, shrink=shrink)[:ax][:tick_params](labelsize=8)
     ax3 = subplot(1,3,3)
     title("Vulnerable", fontsize=8)
     pcm3 = pcolormesh(gridlonBenthos, gridlatBenthos, permutedims(field3, [2,1]), vmin=vmin, vmax=vmax)
@@ -264,5 +362,42 @@ function make_plot_grid3(field1, field2, field3; vmin=0, vmax=1.)
     ax3[:tick_params]("both",labelsize=6)
     gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
     #PyPlot.scatter(obslon, obslat, s=0.5, c=g3log, vmin=0, vmax=10.)
-    colorbar(pcm3)[:ax][:tick_params](labelsize=8)
+    colorbar(pcm3, shrink=shrink)[:ax][:tick_params](labelsize=8)
+end
+
+"""
+```julia-repl
+make_plot_grid_spec(field1, field2, field3, vmin, vmax)
+```
+Create a figure with the 3 fields side-by-side for the specific benthos case
+"""
+function make_plot_grid_spec(field1, field2, field3; vmin=0, vmax=1., shrink=1.)
+    figure("benthos_results", figsize=(12,8))
+    ax1 = subplot(1,3,1)
+    title("Arctica islandica", fontsize=8)
+    pcm1 = pcolormesh(gridlonBenthos, gridlatBenthos, permutedims(field1, [2,1]), vmin=vmin, vmax=vmax)
+    PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax1[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    #PyPlot.scatter(obslon, obslat, s=0.5, c=g1log, vmin=0, vmax=10.)
+    cb = colorbar(pcm1, shrink=shrink, extend="max")[:ax][:tick_params](labelsize=8)
+    add_mask(bx, by, b)
+    ax2 = subplot(1,3,2)
+    title("Spiophanes bombyx", fontsize=8)
+    pcm2 = pcolormesh(gridlonBenthos, gridlatBenthos, permutedims(field2, [2,1]), vmin=vmin, vmax=vmax)
+    PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax2[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    #PyPlot.scatter(obslon, obslat, s=0.5, c=g2log, vmin=0, vmax=10.)
+    colorbar(pcm2, shrink=shrink, extend="max")[:ax][:tick_params](labelsize=8)
+    add_mask(bx, by, b)
+    ax3 = subplot(1,3,3)
+    title("Urothoe poseidonis", fontsize=8)
+    pcm3 = pcolormesh(gridlonBenthos, gridlatBenthos, permutedims(field3, [2,1]), vmin=vmin, vmax=vmax)
+    PyPlot.contourf(bx,by,permutedims(b,[2,1]), levels = [-1e5,0],colors = [[.5,.5,.5]])
+    ax3[:tick_params]("both",labelsize=6)
+    gca()[:set_aspect](1/cos(mean([ylim()...]) * pi/180))
+    #PyPlot.scatter(obslon, obslat, s=0.5, c=g3log, vmin=0, vmax=10.)
+    colorbar(pcm3,shrink=shrink, extend="max")[:ax][:tick_params](labelsize=8)
+    add_mask(bx, by, b)
 end

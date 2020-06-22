b4842d10ad49c01efcd6c1a42dc2d2385da54554
Alexander Barth <barth.alexander@gmail.com>
2020-06-22 20:31:55 +0200

    fix summary

--- src/emodnet_bio_summary.jl
+++ src/emodnet_bio_summary.jl
@@ -1,6 +1,6 @@
 
 function summary(outdir)
-    data = [JSON.parse(String(read(fname))) for fname in sort(glob("*json",outdir))];
+    data = [JSON.parse(String(read(fname))) for fname in sort(glob("*_interp.json",outdir))];
     df = DataFrame(name = getindex.(data,"name"), validation = getindex.(data,"validation"));
     CSV.write(joinpath(outdir,"summary.csv"),df)
     score = mean(df.validation)

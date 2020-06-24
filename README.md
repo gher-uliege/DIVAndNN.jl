39db82e686f52316cb291cac55226e8ee02a26b7
Alexander Barth <barth.alexander@gmail.com>
2020-06-24 14:38:25 +0200

    add some badges

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # DIVAndNN
 
-DIVAnd with neural network which can be installed with the Julia package manager using
+[![Build Status Linux and macOS](https://travis-ci.org/gher-ulg/DIVAndNN.jl.svg?branch=master)](https://travis-ci.org/gher-ulg/DIVAndNN.jl)
+[![documentation latest](https://img.shields.io/badge/docs-latest-blue.svg)](https://gher-ulg.github.io/DIVAndNN.jl/latest/)
+
+DIVAnd with a neural network which can be installed with the Julia package manager using the following command:
 
 
 ```julia
---
+++ test/test_syntetic.jl
@@ -0,0 +1,39 @@
+using DIVAnd
+using Test
+using DIVAndNN
+
+
+# grid of background field
+mask, pmn, xyi = DIVAnd_squaredom(2, range(-1, stop = 1, length = 50))
+
+n = 100
+xy = (zeros(n),zeros(n))
+f = zeros(n)
+f[1:(n÷4)] .= 1
+
+# correlation length
+len = (0.15,0.15)
+epsilon2 = 1.
+
+# no covariables
+field = zeros(size(mask)...,0)
+
+NLayers = []
+
+value_analysis,fw0 = DIVAndNN.analysisprob(
+    mask,pmn,xyi,xy,
+    f,
+    len,epsilon2,
+    field,
+    NLayers,
+    costfun = DIVAndNN.nll,
+    niter = 1000,
+    learning_rate = 0.001,
+    rmaverage = true,
+)
+
+@test minimum(value_analysis) ≈ 1/4 atol=1e-3
+@test maximum(value_analysis) ≈ 1/4 atol=1e-3
+
+
+
